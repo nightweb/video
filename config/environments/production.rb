e6337a53f5a9cf3e250b09f05c62f8ba71dc400b
ytruongdang <ytruongdang@gmail.com>
2015-09-26 01:06:05 +0700

    dsad

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,14 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   #config mailer
-  config.action_mailer.delivery_method = :sendmail
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'example.com',
+    user_name:            'ytdse02415@fpt.edu.vn',
+    password:             'dangy89',
+    authentication:       'plain',
+    enable_starttls_auto: true  
+  }
 end
