c0d64297587aaf833c5914c31532ac360bd531f1
ytruongdang <ytruongdang@gmail.com>
2015-09-25 23:21:38 +0700

    email

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,8 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
   default from: "from@example.com"
   layout 'mailer'
-  def registration_confirmation(user)
-  	@user = user
-  	mail(:to => "#{user.username} <#{user.email}>", :subject => "Registration Confirmation")
-  end
+  
 end
